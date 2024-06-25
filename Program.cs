2a24e8aa0122329c78b2f4d72795216edec729e9
ziv.gliser <zivgl66@gmail.com>
2024-06-25 14:16:11 +0300

    Assd

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Reflection;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Get the version of the assembly.
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            Console.WriteLine($"Version: {version}");
+        }
+    }
+}
